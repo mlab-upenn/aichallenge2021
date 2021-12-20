ae3d434c3ad7f02903233ac57e44b383327cc159
zzangupenn <zzang@seas.upenn.edu>
2021-12-20 04:21:43 -0500

    tar file

--- evaluation/build.sh
+++ evaluation/build.sh
@@ -4,4 +4,4 @@ mkdir -p bringup/scenario
 cp -r ../autoware/adehome/aichallenge_ws/src/* bringup/autoware/adehome/aichallenge_ws/src/
 cp -r ../scenario/* bringup/scenario/
 
-docker build -t ade_foxy_eval .
+docker build -t ade_foxy_eval . --no-cache
